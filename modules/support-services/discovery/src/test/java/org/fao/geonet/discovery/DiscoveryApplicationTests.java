ae426007984f9653ee566bc114f69cfcdc83ed15
Gabriel Roldan <gabriel.roldan@gmail.com>
2020-10-14 11:39:34 -0300

    Initial implementation of discovery-service

---
+++ modules/support-services/discovery/src/test/java/org/fao/geonet/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,19 @@
+/**
+ * (c) 2020 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license,
+ * available at the root application directory.
+ */
+package org.fao.geonet.discovery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiscoveryApplicationTests {
+
+    @Test
+    public void contextLoads() {}
+}
