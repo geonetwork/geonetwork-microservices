7cbeb3617c57b7e12e5464311b88afdf53f4d8e5
Gabriel Roldan <gabriel.roldan@gmail.com>
2020-10-19 14:20:24 -0300

    Enable sample responses in ogc-records-api
    OpenAPI's service interface provide sample responses with
    HTTP status 501. Use them for the sake of manual testing.

--- modules/services/ogc-api-records/service/src/main/java/org/fao/geonet/ogcapi/records/CapabilitiesApiController.java
+++ modules/services/ogc-api-records/service/src/main/java/org/fao/geonet/ogcapi/records/CapabilitiesApiController.java
@@ -7,21 +7,34 @@
 package org.fao.geonet.ogcapi.records;
 
 import io.swagger.annotations.ApiParam;
+import java.util.Optional;
 import org.fao.geonet.ogcapi.records.rest.ogc.model.CollectionInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.context.request.NativeWebRequest;
 
 @Controller
 public class CapabilitiesApiController implements CapabilitiesApi {
 
   /**
+   * Only to support sample responses from {@link CapabilitiesApi}, remove once
+   * all its methods are implemented.
+   */
+  private @Autowired NativeWebRequest nativeWebRequest;
+
+  @Override
+  public Optional<NativeWebRequest> getRequest() {
+    return Optional.of(nativeWebRequest);
+  }
+
+  /**
    * Describe a collection.
    */
   public ResponseEntity<CollectionInfo> describeCollection(
-      @ApiParam(value = "Identifier (name) of a specific collection", required = true)
-      @PathVariable("collectionId")
-      String collectionId) {
+      @ApiParam(value = "Identifier (name) of a specific collection", required = true) 
+      @PathVariable("collectionId") String collectionId) {
     CollectionInfo collectionInfo = new CollectionInfo();
     collectionInfo.name("My GeoNetwork catalog");
     collectionInfo.description("Search for datasets, services and maps.");
