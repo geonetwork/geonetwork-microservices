29f0fe823351bee4c03cef3bdba20784010144dc
Gabriel Roldan <gabriel.roldan@gmail.com>
2020-10-19 13:20:30 -0300

    gateway-service declarative routes configuration

--- modules/services/gateway/src/main/java/org/fao/geonet/gateway/GnGatewayApp.java
+++ modules/services/gateway/src/main/java/org/fao/geonet/gateway/GnGatewayApp.java
@@ -6,17 +6,9 @@
 
 package org.fao.geonet.gateway;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @RefreshScope
@@ -24,31 +16,5 @@ public class GnGatewayApp {
 
   public static void main(String[] args) {
     SpringApplication.run(GnGatewayApp.class, args);
-  }
-
-  @Getter
-  @Setter
-  @Value("${gn.url:http://localhost:8080/geonetwork}")
-  private String geonetworkUrl;
-
-  /**
-   * Register gateway routes.
-   */
-  @Bean
-  public RouteLocator myRoutes(RouteLocatorBuilder builder) throws URISyntaxException {
-
-    String geonetworkWebapp = new URI(geonetworkUrl).getPath();
-
-    return builder.routes()
-      .route("GeoNetwork route", p -> p
-          .path(geonetworkWebapp + "/**")
-          .uri(geonetworkUrl))
-      .route(p -> p
-        .path("/authenticate")
-        .uri("http://127.0.0.1:9998/authenticate"))
-      .route(p -> p
-        .path("/search")
-        .uri("http://127.0.0.1:9990/search"))
-      .build();
   }
 }
