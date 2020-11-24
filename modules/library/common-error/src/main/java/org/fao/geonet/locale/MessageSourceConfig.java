aa50fbb5cc03a18d0b88b3db3f56e56037744090
Francois Prunayre <fx.prunayre@gmail.com>
2020-11-24 17:01:15 +0100

    Only one local resolver.

--- modules/library/common-error/src/main/java/org/fao/geonet/locale/MessageSourceConfig.java
+++ modules/library/common-error/src/main/java/org/fao/geonet/locale/MessageSourceConfig.java
@@ -5,18 +5,15 @@
 
 package org.fao.geonet.locale;
 
-import java.util.Locale;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Component
 public class MessageSourceConfig {
-  /**
-   * Localized message.
-   */
+  //  /**
+  //   * Localized message.
+  //   */
   //  @Bean
   //  public LocaleResolver localeResolver() {
   //    SessionLocaleResolver localeResolver = new SessionLocaleResolver();
