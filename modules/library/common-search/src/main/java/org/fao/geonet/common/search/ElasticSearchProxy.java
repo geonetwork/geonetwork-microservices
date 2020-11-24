7e861bbb893258f1194f068383bb0e9058384b25
Jose García <josegar74@gmail.com>
2020-11-24 15:06:42 +0100

    Add method in ElasticSearchProxy to return the results instead of streaming to the HttpServletResponse

--- modules/library/common-search/src/main/java/org/fao/geonet/common/search/ElasticSearchProxy.java
+++ modules/library/common-search/src/main/java/org/fao/geonet/common/search/ElasticSearchProxy.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -107,58 +108,36 @@ public class ElasticSearchProxy {
       String body,
       String selectionBucket) throws Exception {
 
-    //String name = SecurityContextHolder.getContext().getAuthentication().getName();
-    List<Integer> viewingGroup = new ArrayList<>();
-    List<Integer> editingGroup = new ArrayList<>();
+    UserInfo userInfo = getUserInfo();
 
-    /*if (!name.equalsIgnoreCase("anonymousUser")) {
-      Map claims = (Map) SecurityContextHolder.getContext().getAuthentication().getDetails();
-      viewingGroup = (List<Integer>) claims.get("_viewingGroup");
-      editingGroup = (List<Integer>) claims.get("_editingGroup");
-   }*/
+    String requestBody = processSearchQuery(body, selectionBucket, userInfo);
 
-    UserInfo userInfo = new UserInfo();
-    //userInfo.setUserName(name);
-    userInfo.setViewingGroups(viewingGroup);
-    userInfo.setEditingGroups(editingGroup);
+    handleRequest(httpSession, request, response,
+        requestBody, userInfo, true, selectionBucket);
+  }
 
-    final String url = serverUrl + "/" + defaultIndex + "/_search?";
 
-    ObjectMapper objectMapper = new ObjectMapper();
+  /**
+   * Process the ES request adding additional filters for privileges, etc. and returns the ES
+   * response.
+   *
+   * @param httpSession     Http session.
+   * @param request         Http request.
+   * @param body            Request body.
+   * @param selectionBucket Selection bucket.
+   * @throws Exception Error.
+   */
+  public String searchAndGetResult(
+      HttpSession httpSession,
+      HttpServletRequest request,
+      String body,
+      String selectionBucket) throws Exception {
 
-    // multisearch support
-    final MappingIterator<Object> mappingIterator = objectMapper.readerFor(JsonNode.class)
-        .readValues(body);
-    StringBuffer requestBody = new StringBuffer();
-    while (mappingIterator.hasNextValue()) {
-      JsonNode node = (JsonNode) mappingIterator.nextValue();
-      final JsonNode indexNode = node.get("index");
-      if (indexNode != null) {
-        ((ObjectNode) node).put("index", defaultIndex);
-      } else {
-        final JsonNode queryNode = node.get("query");
-        if (queryNode != null) {
-          addFilterToQuery(objectMapper, node, userInfo);
-          if (selectionBucket != null) {
-            // Multisearch are not supposed to work with a bucket.
-            // Only one request is store in session
+    UserInfo userInfo = getUserInfo();
 
-            // TODO: Review if required
-            //session.setProperty(Geonet.Session.SEARCH_REQUEST + selectionBucket, node);
-          }
-        }
-        final JsonNode sourceNode = node.get("_source");
-        if (sourceNode != null) {
-          final JsonNode sourceIncludes = sourceNode.get("includes");
-          if (sourceIncludes != null && sourceIncludes.isArray()) {
-            ((ArrayNode) sourceIncludes).add("op*");
-          }
-        }
-      }
-      requestBody.append(node.toString()).append(System.lineSeparator());
-    }
-    handleRequest(httpSession, request, response, url,
-        requestBody.toString(), userInfo, true, selectionBucket);
+    String requestBody = processSearchQuery(body, selectionBucket, userInfo);
+
+    return handleRequestAndGetResult(httpSession, request, requestBody, userInfo, true, selectionBucket);
 
   }
 
@@ -209,14 +188,28 @@ public class ElasticSearchProxy {
   }
 
 
+  /**
+   * Query ES and streams the result to HttpServletResponse.
+   *
+   * @param httpSession
+   * @param request
+   * @param response
+   * @param requestBody
+   * @param userInfo
+   * @param addPermissions
+   * @param selectionBucket
+   * @throws Exception
+   */
   private void handleRequest(HttpSession httpSession,
       HttpServletRequest request,
       HttpServletResponse response,
-      String esUrl,
       String requestBody,
       UserInfo userInfo,
       boolean addPermissions,
       String selectionBucket) throws Exception {
+
+    String esUrl = getSearchUrl();
+
     try {
       URL url = new URL(esUrl);
 
@@ -312,37 +305,124 @@ public class ElasticSearchProxy {
         }
 
         try {
-          String acceptHeader = request.getHeader("Accept");
-          Class<? extends SearchResponseProcessor> responseProcessorClass =
-              RESPONSE_PROCESSOR.get(acceptHeader);
-          if (responseProcessorClass == null) {
-            throw new UnsupportedOperationException(String.format(
-                "No response processor configured for '%s'. Use one of %s.",
-                acceptHeader, RESPONSE_PROCESSOR.keySet().toArray()));
-          }
+          processResponse(request, httpSession, streamFromServer, streamToClient,
+              addPermissions, selectionBucket, userInfo);
 
-          SearchResponseProcessor responseProcessor =
-              applicationContext.getBean(responseProcessorClass);
-          if (responseProcessor == null) {
-            throw new UnsupportedOperationException(String.format(
-                "No response processor bean found for '%s'.",
-                acceptHeader));
-          }
-
-          if (responseProcessor instanceof XsltResponseProcessorImpl) {
-            ((XsltResponseProcessorImpl) responseProcessor).setTransformation(
-                ACCEPT_FORMATTERS.get(acceptHeader)
-            );
-          }
-
-          responseProcessor.processResponse(
-              httpSession,
-              streamFromServer, streamToClient,
-              userInfo, selectionBucket, addPermissions);
-          streamToClient.flush();
         } finally {
           IOUtils.closeQuietly(streamFromServer);
         }
+      } catch (Exception ex) {
+        ex.printStackTrace();
+
+        throw new Exception(
+            String.format(
+                "Failed to connect to index at URL %s. %s",
+                esUrl, ex.getMessage()), ex);
+      } finally {
+        connectionWithFinalHost.disconnect();
+      }
+    } catch (IOException e) {
+      // connection problem with the host
+      e.printStackTrace();
+
+      throw new Exception(String.format(
+          "Failed to request index at URL %s. Check configuration.",
+          esUrl), e);
+    }
+  }
+
+  /**
+   * Query ES and returns the result as a String.
+   *
+   * @param httpSession
+   * @param request
+   * @param requestBody
+   * @param userInfo
+   * @param addPermissions
+   * @param selectionBucket
+   * @throws Exception
+   */
+  private String handleRequestAndGetResult(HttpSession httpSession,
+      HttpServletRequest request,
+      String requestBody,
+      UserInfo userInfo,
+      boolean addPermissions,
+      String selectionBucket) throws Exception {
+
+    String esUrl = getSearchUrl();
+    try {
+      URL url = new URL(esUrl);
+
+      // open communication between proxy and final host
+      // all actions before the connection can be taken now
+      HttpURLConnection connectionWithFinalHost = (HttpURLConnection) url.openConnection();
+      try {
+        boolean isSearch = isSearch(request);
+        connectionWithFinalHost.setRequestMethod(
+            isSearch ? "POST" : request.getMethod());
+
+        // copy headers from client's request to request that will be send to the final host
+        copyHeadersToConnection(request, connectionWithFinalHost);
+
+        connectionWithFinalHost.setDoOutput(true);
+        LOGGER.debug(requestBody);
+        connectionWithFinalHost.getOutputStream().write(requestBody.getBytes(Constants.ENCODING));
+
+        // connect to remote host
+        // interactions with the resource are enabled now
+        connectionWithFinalHost.connect();
+
+        // send remote host's response to client
+        String contentEncoding = getContentEncoding(connectionWithFinalHost.getHeaderFields());
+
+        int code = connectionWithFinalHost.getResponseCode();
+        if (code != 200) {
+          InputStream errorDetails = "gzip".equalsIgnoreCase(contentEncoding)
+              ? new GZIPInputStream(connectionWithFinalHost.getErrorStream()) :
+              connectionWithFinalHost.getErrorStream();
+
+          throw new Exception( String.format(
+              "Error is: %s.\nRequest:\n%s.\nError:\n%s.",
+              connectionWithFinalHost.getResponseMessage(),
+              requestBody,
+              IOUtils.toString(errorDetails)));
+        }
+
+        // get content type
+        String contentType = connectionWithFinalHost.getContentType();
+        if (contentType == null) {
+          throw new Exception("Host url has been validated by proxy but content type given by remote host is null");
+        }
+
+        // content type has to be valid
+        if (!isContentTypeValid(contentType)) {
+          if (connectionWithFinalHost.getResponseMessage() != null) {
+            if (connectionWithFinalHost.getResponseMessage().equalsIgnoreCase("Not Found")) {
+              // content type was not valid because it was a not found page (text/html)
+              throw new Exception("Remote host not found");
+            }
+          }
+
+          throw new Exception("The content type of the remote host's response \"" + contentType
+              + "\" is not allowed by the proxy rules");
+        }
+
+        if (!contentType.split(";")[0].equals("application/json")) {
+          addPermissions = false;
+        }
+
+        final InputStream streamFromServer = connectionWithFinalHost.getInputStream();
+        final OutputStream streamToClient = new ByteArrayOutputStream();
+
+        try {
+          processResponse(request, httpSession, streamFromServer, streamToClient,
+              addPermissions, selectionBucket, userInfo);
+
+          return streamToClient.toString();
+        } finally {
+          IOUtils.closeQuietly(streamFromServer);
+        }
+
       } catch (Exception ex) {
         ex.printStackTrace();
 
@@ -475,5 +555,102 @@ public class ElasticSearchProxy {
       }
     }
     return false;
+  }
+
+
+  private String getSearchUrl() {
+    return serverUrl + "/" + defaultIndex + "/_search?";
+  }
+
+  private UserInfo getUserInfo() {
+    //String name = SecurityContextHolder.getContext().getAuthentication().getName();
+    List<Integer> viewingGroup = new ArrayList<>();
+    List<Integer> editingGroup = new ArrayList<>();
+
+    /*if (!name.equalsIgnoreCase("anonymousUser")) {
+      Map claims = (Map) SecurityContextHolder.getContext().getAuthentication().getDetails();
+      viewingGroup = (List<Integer>) claims.get("_viewingGroup");
+      editingGroup = (List<Integer>) claims.get("_editingGroup");
+   }*/
+
+    UserInfo userInfo = new UserInfo();
+    //userInfo.setUserName(name);
+    userInfo.setViewingGroups(viewingGroup);
+    userInfo.setEditingGroups(editingGroup);
+
+    return userInfo;
+  }
+
+  private String processSearchQuery(String body, String selectionBucket, UserInfo userInfo) throws Exception {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    // multisearch support
+    final MappingIterator<Object> mappingIterator = objectMapper.readerFor(JsonNode.class)
+        .readValues(body);
+    StringBuffer requestBody = new StringBuffer();
+    while (mappingIterator.hasNextValue()) {
+      JsonNode node = (JsonNode) mappingIterator.nextValue();
+      final JsonNode indexNode = node.get("index");
+      if (indexNode != null) {
+        ((ObjectNode) node).put("index", defaultIndex);
+      } else {
+        final JsonNode queryNode = node.get("query");
+        if (queryNode != null) {
+          addFilterToQuery(objectMapper, node, userInfo);
+          if (selectionBucket != null) {
+            // Multisearch are not supposed to work with a bucket.
+            // Only one request is store in session
+
+            // TODO: Review if required
+            //session.setProperty(Geonet.Session.SEARCH_REQUEST + selectionBucket, node);
+          }
+        }
+        final JsonNode sourceNode = node.get("_source");
+        if (sourceNode != null) {
+          final JsonNode sourceIncludes = sourceNode.get("includes");
+          if (sourceIncludes != null && sourceIncludes.isArray()) {
+            ((ArrayNode) sourceIncludes).add("op*");
+          }
+        }
+      }
+      requestBody.append(node.toString()).append(System.lineSeparator());
+    }
+
+    return requestBody.toString();
+  }
+
+  private void processResponse(HttpServletRequest request, HttpSession httpSession,
+      InputStream streamFromServer, OutputStream streamToClient,
+      boolean addPermissions, String selectionBucket, UserInfo userInfo) throws Exception {
+
+    String acceptHeader = request.getHeader("Accept");
+    Class<? extends SearchResponseProcessor> responseProcessorClass =
+        RESPONSE_PROCESSOR.get(acceptHeader);
+    if (responseProcessorClass == null) {
+      throw new UnsupportedOperationException(String.format(
+          "No response processor configured for '%s'. Use one of %s.",
+          acceptHeader, RESPONSE_PROCESSOR.keySet().toArray()));
+    }
+
+    SearchResponseProcessor responseProcessor =
+        applicationContext.getBean(responseProcessorClass);
+    if (responseProcessor == null) {
+      throw new UnsupportedOperationException(String.format(
+          "No response processor bean found for '%s'.",
+          acceptHeader));
+    }
+
+    if (responseProcessor instanceof XsltResponseProcessorImpl) {
+      ((XsltResponseProcessorImpl) responseProcessor).setTransformation(
+          ACCEPT_FORMATTERS.get(acceptHeader)
+      );
+    }
+
+    responseProcessor.processResponse(
+        httpSession,
+        streamFromServer, streamToClient,
+        userInfo, selectionBucket, addPermissions);
+    streamToClient.flush();
+
   }
 }
