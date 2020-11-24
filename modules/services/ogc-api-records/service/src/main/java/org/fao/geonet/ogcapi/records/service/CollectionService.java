3784fedec3402e1e5e3fa184e20666b2f989e030
Jose García <josegar74@gmail.com>
2020-11-24 15:38:15 +0100

    Checkstyle.

--- modules/services/ogc-api-records/service/src/main/java/org/fao/geonet/ogcapi/records/service/CollectionService.java
+++ modules/services/ogc-api-records/service/src/main/java/org/fao/geonet/ogcapi/records/service/CollectionService.java
@@ -1,18 +1,23 @@
 package org.fao.geonet.ogcapi.records.service;
 
+import java.util.List;
+import javax.annotation.Nullable;
 import org.fao.geonet.domain.Source;
 import org.fao.geonet.domain.SourceType;
 import org.fao.geonet.repository.SourceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import javax.annotation.Nullable;
-import java.util.List;
+
 
 @Service
 public class CollectionService {
   @Autowired
   private SourceRepository sourceRepository;
 
+  /**
+   * Checks if a collection is defined.
+   *
+   */
   public boolean existsCollection(String collectionId) {
     Source source = retrieveSourceForCollection(collectionId);
 
@@ -20,6 +25,10 @@ public class CollectionService {
   }
 
 
+  /**
+   * Retrieves the Source object related to a collection.
+   *
+   */
   @Nullable
   public Source retrieveSourceForCollection(String collectionId) {
     Source source = null;
@@ -36,17 +45,19 @@ public class CollectionService {
     return source;
   }
 
-
+  /**
+   * Retrieves the ElasticSearch filter related to a collection.
+   *
+   */
   public String retrieveCollectionFilter(Source source) {
     String collectionFilter = "";
 
     if (source.getType() == SourceType.subportal) {
-        collectionFilter = source.getFilter();
+      collectionFilter = source.getFilter();
     } else if (source.getType() == SourceType.harvester) {
       collectionFilter = String.format("+harvesterUuid:\"%s\"", source.getUuid());
     }
 
     return collectionFilter;
   }
-
 }
