cd38d0e8eda9e28a0dc46ebc147f85940fb330b0
christophe mangeat <christophe.mangeat@camptocamp.com>
2020-11-25 09:27:37 +0100

    first thought is was not possible to address static method

--- modules/services/ogc-api-records/service/src/main/java/org/fao/geonet/ogcapi/records/MvcConfigurer.java
+++ modules/services/ogc-api-records/service/src/main/java/org/fao/geonet/ogcapi/records/MvcConfigurer.java
@@ -33,7 +33,7 @@ public class MvcConfigurer {
         .findAll() //
         .stream() //
         .map(Language::getId) //
-        .map(id -> Locale.forLanguageTag(id)) //
+        .map(Locale::forLanguageTag) //
         .filter(Objects::nonNull) //
         .collect(Collectors.toList());
 
