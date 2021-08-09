228adee4aed0051848afbd5ceebca83fafd7c3b3
christophe mangeat <christophe.mangeat@camptocamp.com>
2021-08-09 22:40:29 +0200

    remove dead code

--- modules/services/searching/src/main/java/org/fao/geonet/searching/GnSearchApp.java
+++ modules/services/searching/src/main/java/org/fao/geonet/searching/GnSearchApp.java
@@ -6,18 +6,10 @@
 package org.fao.geonet.searching;
 
 
-import org.fao.geonet.repository.GeonetRepositoryImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.xslt.XsltViewResolver;
 
 @SpringBootApplication
 @RefreshScope
