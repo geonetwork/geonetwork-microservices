442a21bdc3c5c3b951c34202d0ea562630b8d077
christophe mangeat <christophe.mangeat@camptocamp.com>
2021-08-14 22:12:38 +0200

    seems there are figures where many view resolver are available

--- modules/services/ogc-api-records/src/main/java/org/fao/geonet/ogcapi/records/controller/ItemApiController.java
+++ modules/services/ogc-api-records/src/main/java/org/fao/geonet/ogcapi/records/controller/ItemApiController.java
@@ -60,6 +60,7 @@ import org.fao.geonet.ogcapi.records.util.XmlUtil;
 import org.fao.geonet.repository.MetadataRepository;
 import org.fao.geonet.view.ViewUtility;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
@@ -91,6 +92,7 @@ public class ItemApiController {
   @Autowired
   ViewUtility viewUtility;
   @Autowired
+  @Qualifier("xsltViewResolver")
   ViewResolver viewResolver;
   @Autowired
   CollectionService collectionService;
