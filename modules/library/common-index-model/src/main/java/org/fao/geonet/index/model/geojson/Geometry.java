89f94007be93d99d2d6f06a86220d7ecfc062bd9
Francois Prunayre <fx.prunayre@gmail.com>
2022-09-16 07:28:22 +0200

    Checkstyle.

--- modules/library/common-index-model/src/main/java/org/fao/geonet/index/model/geojson/Geometry.java
+++ modules/library/common-index-model/src/main/java/org/fao/geonet/index/model/geojson/Geometry.java
@@ -4,11 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import java.util.List;
 
 @JsonInclude(Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -17,6 +17,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Geometry {
+
   @JsonProperty(value = "coordinates")
   private List<List<List<Double>>> coordinates;
   private String type;
