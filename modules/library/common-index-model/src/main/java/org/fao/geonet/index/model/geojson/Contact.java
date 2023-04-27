6265e62f1fefdb2dde1c9b17392771069b2ff60d
Jose García <josegar74@gmail.com>
2023-04-27 16:13:48 +0200

    GeoJSON output updates to specification

---
+++ modules/library/common-index-model/src/main/java/org/fao/geonet/index/model/geojson/Contact.java
@@ -0,0 +1,33 @@
+package org.fao.geonet.index.model.geojson;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import java.util.List;
+
+@JsonInclude(Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Contact {
+  private String name;
+  private String positionName;
+  private String organization;
+  private Link logo;
+  private String phone;
+  private String email;
+  private Address address;
+  private List<Link> links;
+  // TODO: hoursOfService
+  private String contactInstructions;
+  private List<Role> roles;
+
+
+
+}
